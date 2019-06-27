9be15cb18cbede43a35322ecc7697130657af02a
Wei Jian Gan <weijiangan@outlook.com>
2019-06-27 15:13:14 +0800

    use form validation error for error message

--- djangocms_favicon/models.py
+++ djangocms_favicon/models.py
@@ -1,5 +1,6 @@
 from cms.models.pluginmodel import CMSPlugin
 from colorfield.fields import ColorField
+from django import forms
 from django.utils.translation import ugettext_lazy as _
 from filer.fields.image import FilerImageField
 
@@ -21,6 +22,10 @@ class FaviconPluginModel(CMSPlugin):
 
     def clean(self):
         if self.icon.width < 350 or self.icon.height < 350:
-            raise Exception("Image dimensions must be at least 350px")
+            raise forms.ValidationError(
+                "Selected image dimensions must be at least 350px"
+            )
         if self.icon.width != self.icon.height:
-            raise Exception("Image must be have equal width and height")
+            raise forms.ValidationError(
+                "Selected image must be have equal width and height"
+            )
